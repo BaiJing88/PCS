d4c885236597058c0cd40bc397c05d7d58e7ebd0
PCS Developer <developer@pcs.local>
2026-04-03 19:18:56 +0800

    chore: 清理不必要的文件
    移除:
    - .workbuddy/ (工作记忆目录)
    - 各种临时HTML/JS文件
    - 构建脚本
    - 报告和文档

--- deploy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo 正在部署新构建的jar...
-copy /Y "c:\Users\Administrator\Desktop\PCS\PCS-CentralController\build\libs\pcs-central-controller.jar" "c:\Users\Administrator\Desktop\PCS\test-server\pcs-central-controller.jar"
-echo Central Controller: %ERRORLEVEL%
-copy /Y "c:\Users\Administrator\Desktop\PCS\PCS-Spigot\build\libs\PCS-Spigot-1.0.1.jar" "c:\Users\Administrator\Desktop\PCS\test-server\plugins\PCS-Spigot-1.0.1.jar"
-echo Spigot Plugin: %ERRORLEVEL%
-echo 部署完成
